a63403fc49b0c9fcca15309cc1be2d06631216c2
rafael barroso <rafaelrosabarroso@gmail.com>
2022-01-11 23:17:08 -0300

    colando icone

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,34 @@
-
+import {Reatccomponent as GithubIcon} from './assets/img/github.svg';
 
 
 function App() {
   return (
-   <h1>pagina principal</h1>
+   <header>
+     <nav>
+       <div>
+         <h1>Dsmovie</h1>
+           <a href="https://github.com/barroso3b">
+             <div>
+                <GithubIcon />
+              
+                <p>/barroso3b</p>
+
+             </div>
+
+
+           </a>
+         
+         
+         
+         </div>
+       
+       
+       
+       </nav>
+   </header>
+     
   );
+
 }
 
 export default App;
