29dc6d94a4e2df0c53a0ceef3d94c5ee09d36b9f
rafael barroso <rafaelrosabarroso@gmail.com>
2022-01-13 20:53:46 -0300

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,32 @@
-import Navbar from  "componets/navbar";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import form from 'pages/form';
+import Listing from 'pages/Listing';
+import Navbar from  'componets/navbar';
+
 function App() {
   return (
-   <Navbar />
-     
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+        <Route path=":movieId" element={<form/>} />
+        </Route>
+      </Routes>
+      </BrowserRouter>
   );
-
 }
+         
+         
+              
+        
+      
+    
+  
+
 
 export default App;
---
+++ frontend/src/pages/form/index.tsx
@@ -0,0 +1,10 @@
+function  form(){
+
+    return(
+          <h1>Pagina de formulario</h1>
+
+    );
+      
+}
+
+export default form;
